b1fba98a0d5691ef5f5cc7239628caea59206d8d
Fredrik Leijon <fredrik.leijon@gmail.com>
2014-01-29 14:56:14 +0100

    some more tests for procedure building

--- LimeProxy.Tests/Proxy/LimeWebServiceProxyTests.cs
+++ LimeProxy.Tests/Proxy/LimeWebServiceProxyTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Linq;
+using System.Xml.Linq;
 using FakeItEasy;
 using LimeProxy.Models;
 using LimeProxy.Proxy;
@@ -35,7 +36,57 @@ namespace LimeProxy.Tests.Proxy
         }
 
         [Test]
-        public void ShouldCreateCorrectXmlForAStoredProcedureWithParameters()
+        public void ShouldUseValueTypeProviderToResolveTypeForValue()
+        {
+            var p = A.Fake<IValueTypeProvider>();
+            proxy = new LimeWebServiceProxy(limeWebSerivceClientInvoker, p);
+            var pp = new ProcedureParameters()
+            {
+                Parameters = new[]
+            {
+                new Parameter()
+                {
+                    Name = "@@Foo", Value = 12
+                }, 
+            }
+            };
+
+            proxy.ExecuteStoredProcedure(string.Empty, pp);
+            
+            A.CallTo(() => p.Get(pp.Parameters.First().Value)).MustHaveHappened();
+        }
+
+        [Test]
+        public void ShouldUseResultFromValueTypeProviderWhenBuildingXml()
+        {
+            var p = A.Fake<IValueTypeProvider>();
+            proxy = new LimeWebServiceProxy(limeWebSerivceClientInvoker, p);
+            var pp = new ProcedureParameters()
+            {
+                Parameters = new[]
+                {
+                    new Parameter()
+                    {
+                        Name = "@@Foo",
+                        Value = 12
+                    },
+                }
+            };
+            A.CallTo(() => p.Get(A<object>._)).Returns(99);
+            var name = "csp_some_proc";
+            var expected = new XElement("procedure", new XAttribute("name", name),
+               new XElement("parameter",
+                   new XAttribute("name", "@@Foo"),
+                   new XAttribute("value", 12),
+                   new XAttribute("valuetype", 99)));
+
+            proxy.ExecuteStoredProcedure(name, pp);
+
+            A.CallTo(() => limeWebSerivceClientInvoker.ExecuteProcedure(expected.ToString())).MustHaveHappened();
+        }
+
+        [Test]
+        public void ShouldCreateCorrectXmlForAStoredProcedureWithParameter()
         {
             var p = new ProcedureParameters() { Parameters = new[]
             {
@@ -51,6 +102,39 @@ namespace LimeProxy.Tests.Proxy
                     new XAttribute("name","@@Foo"),
                     new XAttribute("value", 12),
                     new XAttribute("valuetype", 3)));
+
+            proxy.ExecuteStoredProcedure(name, p);
+
+            A.CallTo(() => limeWebSerivceClientInvoker.ExecuteProcedure(expected.ToString())).MustHaveHappened();
+        }
+
+        [Test]
+        public void ShouldCreateCorrectXmlForAStoredProcedureWithParameters()
+        {
+            var p = new ProcedureParameters()
+            {
+                Parameters = new[]
+            {
+                new Parameter()
+                {
+                    Name = "@@Foo", Value = 12
+                }, new Parameter()
+                {
+                    Name = "@@Bar", Value = "troll lol"
+                }, 
+            }
+            };
+            var name = "csp_some_proc";
+
+            var expected = new XElement("procedure", new XAttribute("name", name),
+                new XElement("parameter",
+                    new XAttribute("name", "@@Foo"),
+                    new XAttribute("value", 12),
+                    new XAttribute("valuetype", 3)),
+                new XElement("parameter",
+                    new XAttribute("name", "@@Bar"),
+                    new XAttribute("value", "troll lol"),
+                    new XAttribute("valuetype", 8)));
 
             proxy.ExecuteStoredProcedure(name, p);
 
