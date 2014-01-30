70c0633e0ce31fa2df1619f01005d0b8894bf385
Fredrik Leijon <fredrik.leijon@gmail.com>
2014-01-30 14:10:09 +0100

    Added echo route for simple testing

--- LimeProxy/Modules/ProxyModule.cs
+++ LimeProxy/Modules/ProxyModule.cs
@@ -7,21 +7,20 @@ namespace LimeProxy.Modules
 {
     public class ProxyModule : NancyModule
     {
-        private readonly ILimeWebServiceProxy _limeWebServiceProxy;
-
         public ProxyModule(ILimeWebServiceProxy limeWebServiceProxy) : base("/v1")
         {
-            _limeWebServiceProxy = limeWebServiceProxy;
+            Get["/Echo/{data}"] = x => Response.AsText((string)x.data);
+
             Post["/sp/{name}"] = x =>
             {
                 var param = this.Bind<ProcedureParameters>();
-                return _limeWebServiceProxy.ExecuteStoredProcedure(x.name, param);
+                return limeWebServiceProxy.ExecuteStoredProcedure(x.name, param);
             };
 
             Post["/table/{name}"] = x =>
             {
                 var param = this.Bind<TableQuery>();
-                return _limeWebServiceProxy.QueryTable(x.name, param);
+                return limeWebServiceProxy.QueryTable(x.name, param);
             };
 
             After += ctx => ctx.Response.Headers.Add("Access-Control-Allow-Origin", "*");
